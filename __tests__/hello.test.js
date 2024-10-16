7c132a9af79f9fcb63d60025edc44395cc305bf7
Fabien Letort <fletort@gmail.com>
2024-10-16 14:49:10 +0200

    correctif

--- __tests__/hello.test.js
+++ __tests__/hello.test.js
@@ -1,9 +1,11 @@
 import { expect } from '@jest/globals'
 import { hello_world } from '../src/hello'
 
+const log = jest.spyOn(console, 'log').mockImplementation(() => {})
+
 describe('hello', () => {
   it('say hello to me', () => {
     hello_world()
-    expect(true)
+    expect(log).toHaveBeenCalledWith('Hello World')
   })
 })
